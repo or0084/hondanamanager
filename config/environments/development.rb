b5c9803b618343f3330a43d671c911f3d0975da3
or0084 <ec2-user@ip-172-31-11-235.ap-northeast-1.compute.internal>
2022-07-28 08:21:56 +0000

    [Add] ホストの許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "f68ede809a5e46e5a4012cade4c25822.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
