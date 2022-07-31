bd0d60bfe04bc1796cfbd6a70c4dd0585df88cd2
or0084 <ec2-user@ip-172-31-11-235.ap-northeast-1.compute.internal>
2022-07-31 08:25:59 +0000

    [Add] 投稿詳細画面作成中

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -4,5 +4,13 @@ class Admin::BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to admin_books_path
+  end
+
 end
