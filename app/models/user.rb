4992214a2d32fd5e71821e454b25b067dafda5a2
or0084 <ec2-user@ip-172-31-11-235.ap-northeast-1.compute.internal>
2022-08-02 08:21:31 +0000

    [Add] ログイン機能・ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+     user.password = SecureRandom.urlsafe_base64
+     user.name = 'ゲスト'
+    end
+  end
+
 end
