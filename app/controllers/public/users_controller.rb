6c5fe542d5a85a6ec1e28815f4a11e9af672c688
or0084 <ec2-user@ip-172-31-11-235.ap-northeast-1.compute.internal>
2022-08-03 11:42:34 +0000

    [Add] マイページ追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,8 +1,25 @@
 class Public::UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to mypage_path
+  end
+
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction, :image)
+  end
+
 end
