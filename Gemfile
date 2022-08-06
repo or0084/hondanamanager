0a616544ffdad44c17a03924267017cb863b7ab8
or0084 <ec2-user@ip-172-31-11-235.ap-northeast-1.compute.internal>
2022-08-06 08:24:19 +0000

    [Add] 投稿一覧画面追加

--- Gemfile
+++ Gemfile
@@ -58,3 +58,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'kaminari', '~> 1.2.1'
+
+gem 'net-smtp'
+gem 'net-imap'
+gem 'net-pop'
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,12 +6,12 @@ class Book < ApplicationRecord
 
   has_one_attached :image
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/default-image.jpeg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
